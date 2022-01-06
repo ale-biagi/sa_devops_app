4597d2d3617ee2ad10fc7675379d62148557c3f7
Alessandro Biagi <alessandro.biagi@sap.com>
2022-01-06 21:21:20 +0000

    v1.01

--- app/resources/fiori/webapp/Component.js
+++ app/resources/fiori/webapp/Component.js
@@ -1,7 +1,7 @@
 sap.ui.define(['sap/fe/core/AppComponent'], function (AppComponent) {
     'use strict';
 
-    return AppComponent.extend("sa_devops_app-fiori.Component", {
+    return AppComponent.extend("sa-devops-app-fiori.Component", {
         metadata: {
             manifest: "json"
         }
